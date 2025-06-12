78d5dfb93bf794c6b2f50f8550dbe75ae636301e
KevR98 <ramil.kevin98@gmail.com>
2025-06-12 16:59:15 +0200

    write html, adds js cells and some css style (incomplete)

---
+++ JavaScript/script.js
@@ -0,0 +1,13 @@
+//   CREO LE CELLE
+const generateCells = function () {
+  const cella = document.getElementById('casella');
+  for (i = 1; i <= 79; i++) {
+    const oneCell = document.createElement('div');
+    oneCell.classList.add('cell');
+    const cellValue = document.createElement('h3');
+    cellValue.innerText = i;
+    oneCell.appendChild(cellValue);
+    cella.appendChild(oneCell);
+  }
+};
+generateCells();
