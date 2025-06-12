2f917c4b8b4a2f5a44e8f8a6f10246097afb5f55
KevR98 <ramil.kevin98@gmail.com>
2025-06-12 17:10:58 +0200

    Update script.js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,7 +1,7 @@
 //   CREO LE CELLE
 const generateCells = function () {
   const cella = document.getElementById('casella');
-  for (i = 1; i <= 79; i++) {
+  for (i = 1; i <= 76; i++) {
     const oneCell = document.createElement('div');
     oneCell.classList.add('cell');
     const cellValue = document.createElement('h3');
